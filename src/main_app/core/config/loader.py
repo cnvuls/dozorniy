40343d2147ae544ca77cdf0bb927df21afb73b5e
cnvls <hackhype321@gmail.com>
2026-02-23 19:18:05 +0300

    1.6.6 created _read_json

--- src/main_app/core/config/loader.py
+++ src/main_app/core/config/loader.py
@@ -1,15 +1,17 @@
 from abc import ABC
+import json
+from pathlib import Path
 from pydantic import BaseModel
 from core.config.events import ConfigLoaded, GetConfig
 from core.events import EventBus
 
+CONFIG_PATH = Path("default.json")
 
 class ConfigLoader:
     def __init__(self, bus: EventBus) -> None:
         self._bus: EventBus = bus
         
     async def handle_get_config(self, event: GetConfig):
-        # 1. Берем классы прямо из пришедшего события
         model_cls = event.model_class
         resp_cls = event.response_class
         
@@ -23,7 +25,12 @@ class ConfigLoader:
         
         await self._bus.publish(response_event)
     
-    #TODO: сделать не заглушку
     def _read_json(self):
-        return {}
+        if not CONFIG_PATH.exists():
+            return {}
 
+        try:
+            with open(CONFIG_PATH, "r", encoding="utf-8") as r:
+                return json.load(r)
+        except json.JSONDecodeError:
+            return {}
