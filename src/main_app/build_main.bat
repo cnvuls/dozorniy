c956067e23f9c01292c56d762c284a618f8e117c
cnvls <hackhype321@gmail.com>
2026-03-21 17:08:41 +0300

    2.0.1 ага поверил ну и бредятина

--- src/main_app/build_main.bat
+++ src/main_app/build_main.bat
@@ -1,2 +1,2 @@
 @echo off
-uv run pyinstaller --noconfirm --onedir --name "main_app" --add-data "features;features" --add-data "ui;ui" --add-data "core;core" --add-data "connection;connection" --collect-all "flet" --collect-all "websockets" --collect-all "click" main.py
+uv run pyinstaller --noconfirm --onedir --name "main_app" --add-data "features;features" --add-data "ui;ui" --add-data "core;core" --add-data "connection;connection" --collect-all "flet" --collect-all "websockets" --hidden-import="click" main.py
